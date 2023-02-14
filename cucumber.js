97ac115ea29dff75faa06e5637cf922fc5a03e12
Houssem <86334640+Houssem-Ousji@users.noreply.github.com>
2023-02-14 17:31:30 +0100

    Add files via upload

--- cucumber.js
+++ cucumber.js
@@ -1,6 +1,25 @@
+const common = {
+  require: ["stepDefinitions/*.js"], // to add step definitions files,
+  format: ['progress-bar','html:cucumber-report.html','json:cucumber-report.json'], // to specify your output reports 
+  parallel: 2,
+}
 module.exports = {
   default: {
-    require: ["stepDefinitions/*.js"], // to add step definitions files,
-    format: ['progress-bar','html:cucumber-report.html','json:cucumber-report.json'], // to specify your output reports 
+    ...common,
+    worldParameters: { browser: 'chromium' } 
   },
+  forChromiumTest : {
+    ...common,
+    worldParameters: { browser: 'chromium' } 
+  },
+  forFirefoxTest : {
+    ...common,
+    worldParameters: { browser: 'firefox' } 
+  }
+  // desktop: `--world-parameters '{"device": {"type":"desktop","height":720,"width":1280}}'`,
+  // phone: `--world-parameters '{"device": {"type":"phone","height":568,"width":320}}'`,
+  // tablet: `--world-parameters '{"device": {"type":"tablet","height":1024,"width":768}}'`,
+  // chromium: `--world-parameters '{"browser": "chromium"}'`,
+  // firefox: `--world-parameters '{"browser": "firefox"}'`,
+  // webkit: `--world-parameters '{"browser": "webkit"}'`
 };
--- stepDefinitions/hooks.js
+++ stepDefinitions/hooks.js
@@ -1,11 +1,25 @@
-const { Before, After, setDefaultTimeout } = require("@cucumber/cucumber");
-const { chromium } = require("playwright");
-
+const { Before, After, setDefaultTimeout, AfterStep } = require("@cucumber/cucumber");
+const { chromium,firefox,webkit } = require("playwright");
+const {config} = require("../playwright.config")
 
 setDefaultTimeout(5000 * 2)
 Before(async function () {
+  console.log(this.parameters)
   try {
-    this.browser = await chromium.launch({ headless: false });
+    switch (this.parameters.browser) {
+      case "chromium":
+        this.browser = await chromium.launch(config.browserOptions);
+        break;
+      case "webkit":
+        this.browser = await webkit.launch(config.browserOptions);
+        break;
+      case "firefox":
+        this.browser = await firefox.launch(config.browserOptions);
+        break;
+      default:
+        this.browser = await chromium.launch(config.browserOptions);
+        break;
+    }
     const context = await this.browser.newContext();
     this.page = await context.newPage();
     await this.page.goto("https://www.demoblaze.com/");
@@ -14,6 +28,19 @@ Before(async function () {
     console.log(error.toString())
   }
 });
-After({tags: "@tag1"},async function () {
+
+After(async function ({result,testCaseStartedId,pickle}) {
   this.browser.close()
+  // console.log(pickle)
+});
+
+AfterStep(async function ({result,pickleStep,testStepId}) {
+  const screenshotPath = `screenshots/${result.status + pickleStep.id}.png`
+   await this.page.screenshot({
+    path: screenshotPath,
+    fullPage: true,
+  });
+  // this.attach(String(screenshotPath), 'application/json');
+  this.log("hhhh")
+  // console.log(testCaseStartedId)
 });
